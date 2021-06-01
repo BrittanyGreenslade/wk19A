41b0c752f5caf07f08edb8e2caf52dc8ecbd826f
Brittany <bgreensl@ualberta.ca>
2021-06-01 00:56:20 -0600

    added asking for input after error thrown

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ import division
 import traceback
 # would we normally leave the import traceback after debugging or does it matter?
 # why do I have this _pycache_ folder on the side? I don't know where if came from lol
-try:
-    print("Welcome to the simple calculator please select from the following options:\n1: Addition \n2: Subtraction \n3: Multiplication  \n4: Division")
-    selection = int(input("Please enter your selection:"))
+
+print("Welcome to the simple calculator please select from the following options:\n1: Addition \n2: Subtraction \n3: Multiplication  \n4: Division")
+selection = int(input("Please enter your selection:"))
+
+
+def lilCalculator():
     num_one = float(input("Please enter your first number:"))
     num_two = float(input("Please enter your second number:"))
     if (selection == 1):
@@ -23,5 +26,15 @@ try:
         quot_num = division.divide_things(num_one, num_two)
         print(f"Your result:{quot_num}")
 
+
+# okay I can't figure out how to make a custom exception but this works for now
+
+# def CalculatorInputError():
+#     if ValueError:
+        # print("The calculator only accepts numeric values")
+try:
+    lilCalculator()
 except ValueError:
-    print("Please enter a numeric value")
+    print("The calculator only accepts numeric values")
+# this seems to work but only handles one exception so it doesn't feel like the right way
+    lilCalculator()
